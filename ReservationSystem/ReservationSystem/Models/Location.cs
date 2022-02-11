b32a57b1fc4cfdcde8fd13ec24f8456fb2619f9b
Luke Parker <lukeparke0@gmail.com>
2022-02-10 21:06:34 -0600

    Input boiler plate code

---
+++ ReservationSystem/ReservationSystem/Models/Location.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Models
+{
+    public class Location
+    {
+        [Key]
+        [Required]
+        [Display(Name = "locationId")]
+        public string LocationId { get; set; }
+        [Required]
+        [Display(Name = "locationName")]
+        public string LocationName { get; set; }
+        
+        [Required]
+        [Display(Name = "locationCity")]
+        public string LocationCity { get; set; }
+        [Required]
+        [Display(Name = "locationState")]
+        public string LocationState { get; set; }
+        [Required]
+        [Range(0, 1)]
+        [Display(Name = "locationCapacity")]
+        public int LocationCapacity { get; set; }
+        [Required]
+        [Display(Name = "locationOpenTime")]
+        public DateTime LocationOpenTime { get; set; }
+        [Required]
+        [Display(Name = "locationCloseTime")]
+        public DateTime LocationCloseTime { get; set; }
+
+        [Required]
+        [Display(Name = "reservationList")]
+        public virtual List<Reservation> Reservations { get; set; }
+
+    }
+}
---
+++ ReservationSystem/ReservationSystem/Models/Reservation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Models
+{
+    public class Reservation
+    {
+        [Required]
+        [Display(Name = "reservationId")]
+        public string ReservationId { get; set; }
+
+        [Required]
+        [Display(Name = "locationId")]
+        public string LocationId { get; set; }
+
+        [Required]
+        [Display(Name = "customerId")]
+        public string CustomerId { get; set; }
+
+        [Required]
+        [Display(Name = "length")]
+        public int ReservationLength { get; set; }
+
+        [Required]
+        [Display(Name = "partySize")]
+        public int PartySize { get; set; }
+
+        [Required]
+        [Display(Name = "ReservationDateTime")]
+        public DateTime ReservationDateTime { get; set; }
+
+    }
+}
