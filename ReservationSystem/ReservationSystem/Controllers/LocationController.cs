01c7c0a1a97313952b0263d638cbf676e2d30510
Luke Parker <lukeparke0@gmail.com>
2022-02-11 15:42:42 -0600

    Built Get, Delete, and Post, and seed

---
+++ ReservationSystem/ReservationSystem/Controllers/LocationController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReservationSystem.Daos;
+using ReservationSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/[controller]")]
+    [Produces("application/json")]
+    public class LocationController : ControllerBase
+    {
+        private readonly ReservationSystemDao _dao;
+        public LocationController(ReservationSystemDao dao)
+        {
+            _dao = dao;
+            if (_dao.Locations.Any()) return;
+
+            ReservationSeed.InitData(dao);
+        }
+
+        [HttpGet]
+        [Route("")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<IQueryable<Location>> GetLocations([FromQuery] string locationName)
+        {
+            var result = _dao.Locations as IQueryable<Location>;
+
+            if (!string.IsNullOrEmpty(locationName))
+            {
+                result = result.Where(l => l.LocationName.Contains(locationName, StringComparison.InvariantCultureIgnoreCase));
+            }
+
+            return Ok(result.OrderBy(l => l.LocationId));
+        }
+        //[HttpGet]
+        //[Route("{locationId}")]
+        //[ProducesResponseType(StatusCodes.Status200OK)]
+        //public ActionResult<IQueryable<Location>> GetLocationsById([FromQuery] string locationId)
+        //{
+        //    var result = _dao.Locations as IQueryable<Location>;
+
+        //    if (!string.IsNullOrEmpty(locationId))
+        //    {
+        //        result = result.Where(l => l.LocationId.Contains(locationId, StringComparison.InvariantCultureIgnoreCase));
+        //    }
+
+        //    return Ok(result.OrderBy(l => l.LocationId));
+        //}
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public ActionResult<Location> PostLocation([FromBody] Location location)
+        {
+            try
+            {
+                _dao.Locations.Add(location);
+                _dao.SaveChanges();
+
+                return new CreatedResult($"/locations/{location.LocationId.ToLower()}", location);
+            }
+            catch (Exception e)
+            {
+                return ValidationProblem(e.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("{locationId}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Location> DeleteLocation([FromRoute] string locationId)
+        {
+            try
+            {
+                var locationList = _dao.Locations as IQueryable<Location>;
+                var location = locationList.First(l => l.LocationId.Equals(locationId));
+                _dao.Locations.Remove(location);
+                _dao.SaveChanges();
+
+                return new CreatedResult($"/locations/{location.LocationId.ToLower()}", location);
+            }
+            catch (Exception e)
+            {
+                return ValidationProblem(e.Message);
+            }
+        }
+    }
+}
---
+++ ReservationSystem/ReservationSystem/Models/Brand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Models
+{
+    public class Brand
+    {
+        [Key]
+        [Required]
+        [Display(Name = "brandId")]
+        public string BrandId { get; set; }
+        [Required]
+        [Display(Name = "brandName")]
+        public string BrandName { get; set; }
+
+        [Required]
+        [Display(Name = "locations")]
+        public virtual List<Location> Locations { get; set; }
+    }
+}
